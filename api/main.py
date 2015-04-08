8508050a9c804dd1736510283ae8aaa6801ea4df
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-04-08 07:51:58 -0700

    added request handler for POSTed json, which will be filled out later to handle posts from the javascript in the eextension

--- api/main.py
+++ api/main.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def root():
+    """Handle requests for app root."""
+    return 'Root of the application.'
 
+@app.route('/pie', methods=['POST'])
+def receive_data():
+    """Receive date from an ajax call."""
+    # test with: 
+    # curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz""password":"xyz"}' http://localhost:9080/pie
+    json_received = request.get_json()
+    print json_received
+    return jsonify(json_received)
 
 @app.errorhandler(404)
 def page_not_found(e):
