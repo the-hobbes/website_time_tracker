4f5d19e532c463959ece0c20331f5e51e7e12e4b
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-04-09 09:06:51 -0700

    Working on massaging the data into proper json format.

--- top_10.js
+++ top_10.js
@@ -98,6 +98,47 @@ var printTopResults = function(topResults, allResults) {
   createPieChart(topResults, allResults); 
 }
 
+var parseToJson = function(allResults) {
+  var results_string = JSON.stringify(allResults);
+  var data = JSON.parse(results_string);
+
+  return [
+      { 
+        "label": "One",
+        "value" : 29.765957771107
+      } , 
+      { 
+        "label": "Two",
+        "value" : 0
+      } , 
+      { 
+        "label": "Three",
+        "value" : 32.807804682612
+      } , 
+      { 
+        "label": "Four",
+        "value" : 196.45946739256
+      } , 
+      { 
+        "label": "Five",
+        "value" : 0.19434030906893
+      } , 
+      { 
+        "label": "Six",
+        "value" : 98.079782601442
+      } , 
+      { 
+        "label": "Seven",
+        "value" : 13.925743130903
+      } , 
+      { 
+        "label": "Eight",
+        "value" : 5.1387322875705
+      }
+    ];
+
+}
+
 var createPieChart = function(topResults, allResults) {  
   // for(var key in obj) {
   //   var attrName = key;
@@ -108,51 +149,23 @@ var createPieChart = function(topResults, allResults) {
   // key/value pairs each: {"url":"www...", "count:x"}, like:
   // var data=[{"crimeType":"mip","totalCrimes":24},{"crimeType":"theft","totalCrimes":558}];    
   
-  var results_string = JSON.stringify(allResults);
-  var data = JSON.parse(results_string);
+  data = parseToJson(allResults);
 
   console.log('stop here');
 
-  var width = 960,
-      height = 500,
-      radius = Math.min(width, height) / 2;
+  nv.addGraph(function() {
+  var chart = nv.models.pieChart()
+      .x(function(d) { return d.label })
+      .y(function(d) { return d.value })
+      .showLabels(true);
 
-  var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    d3.select("#chart svg")
+        .datum(exampleData())
+        .transition().duration(350)
+        .call(chart);
 
-  var arc = d3.svg.arc()
-      .outerRadius(radius - 10)
-      .innerRadius(0);
-
-  var pie = d3.layout.pie()
-      .sort(null)
-      .value(function(d) { return d.population; });
-
-  var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-
-  data.forEach(function(d) {
-    d.population = +d.population;
+    return chart;
   });
-
-  var g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
-
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.age); });
-
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.age; });
 
 }
 
