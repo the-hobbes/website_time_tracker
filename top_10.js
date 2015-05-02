5880b70ad5307e7c66f82f8c8d5bdf70d201ec0a
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-05-02 09:09:37 -0700

    Removed call to shadow filter to keep master branch clean

--- top_10.js
+++ top_10.js
@@ -216,19 +216,11 @@ var createPieChart = function(pieChartData) {
 
   chart.legend.margin({top: 5, right:50, left:0, bottom: 0});
 
-  // LEAVE OUT DROPSHADOW FOR NOW
-  // createDropShadowFilter();
-
-  // d3.selectAll("svg")
-  // .attr("class", "shadow")
-  // .attr("filter", "url(#dropshadow)");
-
   d3.select("#chart svg")
     .datum(data)
     .transition().duration(1200)
     .call(chart);
     
-  document.getElementsByClassName("shadow").removeAttribute("filter");
   return chart;
   });
 }
