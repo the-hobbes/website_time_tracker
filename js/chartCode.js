b708bda785349a4624c80750d4241fbc05d1506e
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-05-17 21:11:02 -0700

    Break monolithic JS file into logical parts.
    For issue #20, this change breaks the huge single JS file into many parts and
    imports them separately.

---
+++ js/chartCode.js
@@ -0,0 +1,145 @@
+/**
+  * File containing code to prepare and render the charts.
+  */ 
+
+var printTopResults = function(sortedUrlArray, visitObject) {
+  /**
+   * printTopResults()
+   * Displays urls and counts. Also calls pie chart code.
+   * @param {array} sortedUrlArray An array of count-sorted Urls. 
+   * @param {object} visitObject An object containing url to count mappings.
+   */
+  pieChartData = new Array();   // object to store pie chart data
+  timeseriesData = new Array(); // object to store the timeseries data
+
+  for (var i =0; i < TOP_X; i++) {
+    url = sortedUrlArray[i];
+    count = visitObject[sortedUrlArray[i]].count;
+
+    // make link elements
+    var linkElement = document.createElement('a');
+    linkElement.href = 'http://' + url;
+    linkElement.text = url;
+    linkElement.target = 'blank';
+
+    // create an li for each site, and append the link created above
+    var lineItemElement = document.createElement('li');
+    lineItemElement.appendChild(linkElement);
+    // add the count to the li
+    var countText = document.createTextNode(', ' + count + ' times.');
+    lineItemElement.appendChild(countText);
+
+    // append each line item to the appropriate ol element
+    var orderedListElement = document.getElementById(TOP_SITES_LIST_ID);
+    orderedListElement.appendChild(lineItemElement);
+
+    // this is massaged data for the pie chart
+    var tmp = new Object();
+    tmp.label = url;
+    tmp.value = count;
+    pieChartData.push(tmp);
+
+    // timeseries data should be the top X objects from visitObject
+    timeseriesData.push(visitObject[url])
+  }
+  createPieChart(pieChartData);
+  createTimeseries(timeseriesData);
+  hideLoadingIcon();
+}
+
+var createDropShadowFilter = function() {
+ /**
+   * createDropShadowFilter()
+   * Creates an SVG drop shadow filter for the pie chart.
+   */
+  var svg = d3.select('svg');
+  var defs = svg.append("defs");
+
+  var filter = defs.append("filter")
+      .attr("id", "dropshadow")
+
+  filter.append("feGaussianBlur")
+      .attr("in", "SourceAlpha")
+      .attr("stdDeviation", 4)
+      .attr("result", "blur");
+  filter.append("feOffset")
+      .attr("in", "blur")
+      .attr("dx", 2)
+      .attr("dy", 2)
+      .attr("result", "offsetBlur");
+
+  var feMerge = filter.append("feMerge");
+
+  feMerge.append("feMergeNode")
+      .attr("in", "offsetBlur")
+  feMerge.append("feMergeNode")
+      .attr("in", "SourceGraphic");
+}
+
+var createPieChart = function(pieChartData) {  
+  /**
+   * createPieChart()
+   * Creates a pie chart with url data, using nvd3 libraries.
+   * @param {object} pieChartData An object containing url-to-count mappings
+   *     formatted for the nvd3 pie chart library.
+   */
+  var data = pieChartData;
+  nv.addGraph(function() {
+  var chart = nv.models.pieChart()
+    .x(function(d) { return d.label })
+    .y(function(d) { return d.value })
+    .showLegend(true)
+    .pieLabelsOutside(false)
+    .labelType("percent")
+    .tooltipContent(function(key, y, e, graph) { return key + ', ' + 
+      e.value + ' visits.' })
+    .labelThreshold(.05)
+    .showLabels(true);
+
+  chart.legend.margin({top: 5, right:50, left:0, bottom: 0});
+
+  d3.select("#chart svg")
+    .datum(data)
+    .transition().duration(1200)
+    .call(chart);
+    
+  return chart;
+  });
+}
+
+var createTimeseries = function(timeseriesData) {
+  /**
+   * createTimeseries()
+   * Creates a timeseries with url data, using nvd3 libraries.
+   * @param {object} timeseriesData An object containing url-to-count mappings
+   *     as well as dates, formatted for the nvd3 pie chart library.
+   */
+
+  var chart = nv.models.lineChart()
+                  .useInteractiveGuideline(true)
+                  .tooltips(false)
+                  .showLegend(true)
+                  .showYAxis(true)
+                  .showXAxis(true);
+
+  chart.xAxis
+      .orient("bottom")
+      .tickFormat(function(d) {
+          return d3.time.format('%d-%b-%y')(new Date(d))
+      })
+      .rotateLabels(-45);
+
+  chart.yAxis
+      .axisLabel('Number of Visits')
+      .orient("left")
+      .tickFormat(d3.format('d'));
+
+  var myData = timeseriesData;      
+
+  // render the chart
+  d3.select('#timeseries-chart svg')
+      .datum(myData)
+      .call(chart)
+
+  return chart;
+}
---
+++ js/chromeHistory.js
@@ -0,0 +1,130 @@
+/**
+  * File containing interactions with the chrome history API. 
+  */ 
+
+function buildHistoryItemList(timeslice) {
+  /**
+   * buildHistoryItemList()
+   * This function builds a list of history items from the Chrome history api.
+   * @param {string} timeslice A string indicating which of 3 timeslice 
+   *     durations to use. Note that the if the timeslice is undefined, a 
+   *     default of '1' is used, indicating the duration of a week. 
+   */
+  showLoadingIcon();
+  
+  if (typeof timeslice === 'undefined') { timeslice = WEEK; }
+  var searchDepth = 0
+
+  if (timeslice == WEEK) {
+    var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+    searchDepth = (new Date).getTime() - millisecondsPerWeek;
+  } else if (timeslice == MONTH) {
+    var millisecondsPerMonth = 1000 * 60 * 60 * 24 * 30;
+    searchDepth = (new Date).getTime() - millisecondsPerMonth; 
+  } else {
+    var millisecondsPerYear = 1000 * 60 * 60 * 24 * 360;
+    searchDepth = (new Date).getTime() - millisecondsPerYear; 
+  }
+
+   /* Track the number of callbacks from chrome.history.getVisits()
+    *     that we expect to get.  When it reaches zero, we have all results.
+    */
+  var numRequestsOutstanding = 0;
+
+  chrome.history.search({
+    'text': '',                // Return every history item...
+    'startTime': searchDepth,  // accessed less than x time ago.
+    'maxResults': 0            // 0 == all results
+    },
+   function(historyItems) {
+    // For each history item, get details on all visits.
+    for (var i = 0; i < historyItems.length; ++i) {
+      var url = historyItems[i].url;
+      var processVisitsWithUrl = function(url, historyItem) {
+        /**
+          * processVisitsWithUrl()
+          * We need the url of the visited item to process the visit. Use a
+          *     closure to bind the url into the callback's args.
+          * @param {string} the url of the historyItem.
+          * @param {object} the historyItem object corresponding to the url.
+          * @return {function} A closure to bind url+callback.
+          */
+        return function(visitItems) { 
+          processVisits(url, visitItems, historyItem); };
+      };
+      chrome.history.getVisits({url: url}, 
+        processVisitsWithUrl(url, historyItems[i]));
+      numRequestsOutstanding++;
+    }
+    if (!numRequestsOutstanding) {
+      onAllVisitsProcessed();
+    }
+  });  
+
+  // Maps URLs to a count of the number of times the user visited that URL
+  var visitObject = {};
+  // to be sorted by url count
+  var sortedUrlArray = [];
+
+  var processVisits = function(url, visitItems, historyItem) {
+    /**
+      * processVisits()
+      * Callback for chrome.history.getVisits().  Counts the number of times a
+      *     user visited a URL.
+      * @param {string} url The url to count.
+      * @param {object} the historyItem object corresponding to the url.
+      * @param {object} visitItems The object from the API representing a visit.
+      */
+    for (var i = 0, ie = visitItems.length; i < ie; ++i) {
+      // get simple domains from a given url
+      var rootDomain = new URL(url).hostname;
+      
+      timeOfVisit = historyItem.lastVisitTime
+
+      // we have a new, previously unseen rootDomain
+      if (!visitObject[rootDomain]) {
+        visitObject[rootDomain] = {
+          'key'    : rootDomain,
+          'count'  : 1, // this is the total count of all visits across all times
+          'values' : [{'series':rootDomain, 'x': timeOfVisit, 'y':1}],
+        };
+        sortedUrlArray.push(rootDomain);
+      }
+
+      lastIndex = visitObject[rootDomain].values.length - 1
+
+      // a new, previously unseen time for a rootDomain visit 
+      if (visitObject[rootDomain].values[lastIndex]['x'] == timeOfVisit) {
+        visitObject[rootDomain].values[lastIndex]['y'] += 1
+      } else { // update an existing rootDomain visit
+        var newVisit = {'series':rootDomain, 'x': timeOfVisit, 'y':1}
+        visitObject[rootDomain].values.push(newVisit);
+      }
+
+      // either way, update the rootDomain visit count
+      visitObject[rootDomain].count++;
+    }
+
+     /* If this is the final outstanding call to processVisits(),
+      *     then we have the final results.  Use them to build the list
+      *     of URLs to show in the popup.
+      */
+    if (!--numRequestsOutstanding) { // if you can no longer decrement...
+      onAllVisitsProcessed();
+    }
+  };
+  
+  var onAllVisitsProcessed = function() {
+    /**
+      * onAllVisitsProcessed()
+      * Sort the URLs by the number of times the user typed them, then call
+      *     the function to print them. This is called when we have the final 
+      *     list of URls to display.
+      */
+   sortedUrlArray.sort(function(a, b) {
+     return visitObject[b].count - visitObject[a].count;
+   });
+
+   printTopResults(sortedUrlArray, visitObject);  
+  };
+}
